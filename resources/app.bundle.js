96bf3053128849a81f16a7494742496a3a5cb63e
Alesia <semka.alesya@gmail.com>
2018-02-14 16:28:02 +0300

    correct createMonth(only current)

--- resources/app.bundle.js
+++ resources/app.bundle.js
@@ -2456,16 +2456,15 @@ const daysOfTheWeek = ['пн', 'вт', 'ср', 'чт', 'пт', 'суб', 'вс']
             this.currentMonthInfo.index = month.infoMonth()[0];
             this.currentMonthInfo.year = month.infoMonth()[1];
             this.currentMonthInfo.name = name_months[this.currentMonthInfo.index];
-            this.currentMonthInfo.first = month.determineDayOfWeekOfFirstDay();
             return this.currentMonthInfo;
         }
     },
     methods: {
         selectDay(event) {
             this.daysCalendar.forEach(day => {
-                day[2] = false;
-                if (day[0] == +event.target.attributes.day_num.value) {
-                    day[2] = true;
+                day.selected = false;
+                if (day.val == +event.target.attributes.day_num.value) {
+                    day.selected = true;
                 }
             });
             this.selectedForm.date = event.target.attributes.day_num.value;
@@ -2473,140 +2472,96 @@ const daysOfTheWeek = ['пн', 'вт', 'ср', 'чт', 'пт', 'суб', 'вс']
         }
     }
 });
-// var Day = function(val, currentMonth, currentDay) {
-//     // Приватная функция
-//     var log = function(message) {
-//         console.log(message);
-//     };
-//     // Публичное свойство
-//     this.val = val;
-//     this.currentDay = this.currentDay;
-//     this.currentMonth = currentMonth;
-//     this.selectDay = selectDay;
-//     this.currentDay = currentDay;
-//     this.notEvents = notEvents;
-
-//     // Публичный метод
-//     this.logger = function(message) {
-//         log('Public ' + message);
-//     };
-// };
+var cerrentNumbMonth = new Date().getDate();
 class Day {
-    constructor(index, rule) {
-        //     this.val = val;
-        //     this.currentDay = this.currentDay;
-        //    // this.currentMonth = currentMonth; // не забыть проверить.если это текущий месяц, то искать текущий день
-        //     this.selectDay = selectDay;
-        //     this.notEvents = notEvents;
-        // this.rule = rule;
-        this.rule = rule;
-    }
-    day_generator() {
-        if (this.rule == "prev") {
-            return function () {
-                // arr.sort(a,b);
-                console.log('this.msg', this.msg);
-            };
-        } else if (this.rule == "next") {
-            return function () {
-                // arr.sort(a,b);
-                console.log('this.msg', this.msg);
-            };
+    constructor(val, currentMonth) {
+        this.val = val;
+        this.selected = false;
+        this.notEvents = !currentMonth;
+        if (currentMonth) {
+            if (cerrentNumbMonth == val) {
+                this.currentDay = true;
+            }
         } else {
-            return function () {
-                // arr.sort(a,b);
-                console.log('this.msg', this.msg);
-            };
+            this.currentDay = false;
         }
+    }
+    day_create(currentMonth) {
+        return this;
+    }
+}
+function day_generator(flag_rule) {
+    if (flag_rule == "prev") {
+        return function () {
+            let countsDaysPrevMonth = new Date(this.year, this.index, 0).getDate(); //-- ко-во дней прошлого месяца
+            let temp = getDaysPrevMonth(this.first_day_of_the_week);
+            function getDaysPrevMonth(firstDay) {
+                let tempDays = [];
+                for (let l = firstDay - 1; l >= 0; l--) {
+                    tempDays.push(new Day(countsDaysPrevMonth - l, false).day_create());
+                }
+                return tempDays;
+            }
+            return temp;
+        };
+    } else if (flag_rule == "next") {
+        return function () {
+            let tempLast = getDaysNextMonth(this.last_day_of_the_week);
+            function getDaysNextMonth(lastDay) {
+                let tempDays = [];
+                for (let l = 1; l <= 7 - lastDay; l++) {
+                    tempDays.push(new Day(l, false).day_create());
+                }
+                return tempDays;
+            }
+            return tempLast;
+        };
+    } else {
+        return function () {
+            let tempDays = [];
+            for (let i = 1; i <= this.countsDays; i++) {
+                tempDays.push(new Day(i, true).day_create());
+            }
+            return tempDays;
+        };
     }
 }
 var getMonthIndex = new Date().getMonth() + 0;
-var prevMonthT = new Day(getMonthIndex, 'prev').day_generator();
+var prevMonthT = day_generator('prev');
+var nextMonthT = day_generator('next');
+var curMonthT = day_generator('current');
 
 class Month {
     constructor(index, rule) {
-        this.name = '';
         this.index = index;
-        this.countsDays = 0;
-        this.days = [];
-        this.previousDaysMonth = [];
-        this.nextDaysMonth = [];
         this.year = new Date().getFullYear();
-        this.msg = 'ура ура урара';
-        // this.rule = rule;
+        this.countsDays = new Date(this.year, this.index + 1, 0).getDate(); //количество дней в текущем месяце
+        this.first_day_of_the_week = new Date(this.year, this.index).getDay() - 1; //-- день недели первого дня месяца
+        this.last_day_of_the_week = new Date(this.year, this.index, this.countsDays).getDay(); //-- день недели последнего дня месяца
+        if (this.first_day_of_the_week < 0) {
+            //-- проверка если вск
+            this.first_day_of_the_week = this.first_day_of_the_week + 7;
+        }
         this.createDays = rule.bind(this);
     }
 
     createMonth() {
-        this.countsDays = new Date(this.year, this.index + 1, 0).getDate(); //количество дней в текущем месяце
-
-        let tempDays = [];
-        var cerrentNumbMonth = new Date().getDate();
-
-        for (let i = 1; i <= this.countsDays; i++) {
-            let currentDay = false;
-            let selectDay = false;
-            let notEvents = false;
-            if (cerrentNumbMonth == i) {
-                currentDay = true;
-            }
-            tempDays.push([i, currentDay, selectDay]);
-        }
-        //------------------------------------
-        this.daysCalendar = this.determineDayOfWeekOfFirstDay(tempDays);
+        var month_prev_days = monthPrevDay.createDays();
+        var month_next_days = monthNextDay.createDays();
+        var month_days = month.createDays();
+        this.daysCalendar = month_prev_days.concat(month_days, month_next_days);
         return this.daysCalendar;
     }
     infoMonth() {
         let informMonth = [this.index, this.year];
         return informMonth;
     }
-
-    //-------------------------------------------
-    determineDayOfWeekOfFirstDay(tD) {
-        console.log(this.year, this.index);
-        let first_day_of_the_week = new Date(this.year, this.index).getDay() - 1; //-- день недели первого дня месяца
-        if (first_day_of_the_week < 0) {
-            //-- проверка если вск
-            first_day_of_the_week = first_day_of_the_week + 7;
-        }
-        let tempCountsD = new Date(this.year, this.index, 0).getDate(); //-- ко-во дней
-        let lastDay_of_the_week = new Date(this.year, this.index, this.countsDays).getDay(); //-- день недели последнего дня месяца
-        console.log("lastDay_of_the_week", lastDay_of_the_week);
-        console.log("first_day_of_the_week", first_day_of_the_week);
-        let temp = getDaysPrevMonth(first_day_of_the_week);
-        function getDaysPrevMonth(firstDay) {
-            let tempDays = [];
-            for (let l = firstDay - 1; l >= 0; l--) {
-                let currentDay = false;
-                let selectDay = false;
-                let notEvents = true;
-                tempDays.push([tempCountsD - l, currentDay, selectDay, notEvents]);
-            }
-            return tempDays;
-        }
-        let tempLast = getDaysNextMonth(lastDay_of_the_week);
-        function getDaysNextMonth(lastDay) {
-            let tempDays = [];
-            for (let l = 1; l <= 7 - lastDay; l++) {
-                let currentDay = false;
-                let selectDay = false;
-                let notEvents = true;
-                tempDays.push([l, currentDay, selectDay, notEvents]);
-            }
-            return tempDays;
-        }
-        this.daysCalendar = temp.concat(tD, tempLast);
-
-        return this.daysCalendar;
-    }
-
 }
 
-const month = new Month(getMonthIndex, prevMonthT);
-// const month = new Month(2);
-//создать функцию получения индекса месяца
+const monthPrevDay = new Month(getMonthIndex, prevMonthT);
+const monthNextDay = new Month(getMonthIndex, nextMonthT);
+const month = new Month(getMonthIndex, curMonthT);
 const myMonth = month.createMonth();
-month.createDays();
 
 //------------ apply,call,bind     
 
@@ -21857,13 +21812,13 @@ var render = function() {
                 "p",
                 {
                   class: {
-                    "day-current": day[1],
-                    "day-selected": day[2],
-                    "day-no-events": day[3]
+                    "day-current": day.currentDay,
+                    "day-selected": day.selected,
+                    "day-no-events": day.notEvents
                   },
-                  attrs: { day_num: day[0] }
+                  attrs: { day_num: day.val }
                 },
-                [_vm._v(_vm._s(day[0]))]
+                [_vm._v(_vm._s(day.val))]
               )
             ]
           )
